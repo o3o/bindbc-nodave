e869ef6cb0502669a76fadfd382061fff4ee778a
Orfeo Da Vià <git@orfeo.fastmail.com>
2021-10-07 16:37:16 +0200

    chore: add files

---
+++ src/bindbc/nodave/package.d
@@ -0,0 +1,13 @@
+
+//          Copyright 2021 - 2024 Orfeo Da Vià
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+module bindbc.nodave;
+
+public import bindbc.nodave.types;
+
+version(BindBC_Static) version = BindNodave_Static;
+version(BindNodave_Static) public import bindbc.nodave.bindstatic;
+else public import bindbc.nodave.binddynamic;
